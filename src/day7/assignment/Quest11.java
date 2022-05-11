a57beb792bba770c657922334638208f3eab5a20
rlama890 <rlama890@wuv.edu>
2022-05-10 20:40:15 -0400

    rafactored code

--- src/day7/assignment/Quest11.java
+++ src/day7/assignment/Quest11.java
@@ -2,43 +2,39 @@ package day7.assignment;
 
 import java.util.Scanner;
 
-
 //Write a program to swap two numbers 
 //a) using temporary variable 
 public class Quest11 {
 
 	public static void main(String[] args) {
-		
+		// taking input from scanner
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a :");
 		int a = input.nextInt();
 		System.out.println("Enter b");
-        int b = input.nextInt();
-        
-        int temp = a;
-        a = b;
-        b = temp;
-        System.out.println("The Value of a :" +a);
-        System.out.println("The Value of b :" +b);
-        
-        //b) without temp variable 
-        System.out.println("Enter a :");
-        a = input.nextInt();
+		int b = input.nextInt();
+
+		int temp = a;
+		a = b;
+		b = temp;
+		System.out.println("The Value of a :" + a);
+		System.out.println("The Value of b :" + b);
+
+		// b) without temp variable
+		System.out.println("Enter a :");
+		a = input.nextInt();
 		System.out.println("Enter b");
-        b = input.nextInt();
-        
-        a = a + b;
-        b = a - b;
-        a = a - b;
-        
-       System.out.println("The Value of a is :" +a);
-       System.out.println("The Value of b is :" +b);
- 
-        input.close();
-        		
-        
-        
-		
+		b = input.nextInt();
+
+		a = a + b;
+		b = a - b;
+		a = a - b;
+
+		System.out.println("The Value of a is :" + a);
+		System.out.println("The Value of b is :" + b);
+
+		// closing the scanner
+		input.close();
 	}
 
 }
